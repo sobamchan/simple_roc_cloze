eaf40b2045333c901115816b87a1c1b913acef2c
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2018-09-04 20:21:13 +0900

    dataloader

--- data.py
+++ data.py
@@ -1,12 +1,16 @@
+from pathlib import Path
 import pandas as pd
 from nltk import word_tokenize
 
+from torch.utils import data
+from torch.utils.data.sampler import RandomSampler
+
 
 def prepro(s):
-    return ' '.join(word_tokenize(s))
+    return ' '.join(word_tokenize(s)).lower()
 
 
-class ValidationData:
+class ValidationFormatData(data.Dataset):
 
     def __init__(self, dpath):
         df = pd.read_csv(dpath)
@@ -25,3 +29,30 @@ class ValidationData:
         opt2s = [prepro(s)
                  for s in df.RandomFifthSentenceQuiz2]
         self.opts = list(zip(opt1s, opt2s))
+
+        self.answers = df.AnswerRightEnding.value.tolist()
+
+    def __len__(self):
+        return len(self.answers)
+
+    def __getitem__(self, idx):
+        return {
+                'stories': self.stories[idx],
+                'options': self.opts[idx],
+                'answers': self.answers[idx],
+                }
+
+
+def get_loader(data_dir, batch_size):
+    data_dir = Path(data_dir)
+    train_ds = ValidationFormatData(data_dir / 'train.csv')
+    test_ds = ValidationFormatData(data_dir / 'test.csv')
+
+    train_loader = data.DataLoader(train_ds,
+                                   batch_size=batch_size,
+                                   sampler=RandomSampler(train_ds))
+    test_loader = data.DataLoader(test_ds,
+                                  batch_size=batch_size,
+                                  sampler=RandomSampler(test_ds))
+
+    return train_loader, test_loader
